6a5fdb296588f564b3dcfbeb97cdb59370a88df6
Gold240sx <240designworks@gmail.com>
2023-08-21 09:55:42 -0500

    init commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,14 +6,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
 	title: "Bike Shop",
-	description: "A Next 13 E-Commerce app built with Next.Js,  Tailwind, Supabas, and Stripe.",
+	description: "A Next 13 E-Commerce app built with Next.Js,  Tailwind, Supbase, and Stripe",
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
 			<body className={inter.className}>{children}</body>
