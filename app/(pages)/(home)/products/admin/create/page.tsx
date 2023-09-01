d00bc8e40ee557c4c9b21c5f33290a9e836406bc
Gold240sx <240designworks@gmail.com>
2023-08-31 21:33:55 -0500

    Role based auth completed

--- app/(pages)/(home)/products/admin/create/page.tsx
+++ app/(pages)/(home)/products/admin/create/page.tsx
@@ -1,6 +1,23 @@
 import CreateForm from "./CreateForm"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
 
 export default async function CreateProduct() {
+	const supabase = createServerComponentClient({ cookies })
+
+	const { data: activeSession } = await supabase.auth.getSession()
+
+	if (!activeSession.session) {
+		return redirect("/signin")
+	}
+
+	const { data: user } = await supabase.from("user").select("*").match({ id: activeSession.session.user.id }).single()
+
+	if (user?.role !== "admin") {
+		return redirect("/")
+	}
+
 	return (
 		<main className=" pt-28 main">
 			<h2 className="text-center">Create a New Product</h2>
